82128b29b02072190a824d758eb1aebbc723ca8a
Codingwithnicotine <casanajemcarlo@gmail.com>
2026-03-22 11:48:21 +0800

    friendship_pkey fixes

--- migrations/2026-03-20_friend_pair_state.sql
+++ migrations/2026-03-20_friend_pair_state.sql
@@ -1,22 +1,24 @@
 BEGIN;
 
-UPDATE public.friendships
-SET
-  user_id = LEAST(user_id, friend_user_id),
-  friend_user_id = GREATEST(user_id, friend_user_id)
-WHERE user_id > friend_user_id;
-
-DELETE FROM public.friendships
-WHERE user_id = friend_user_id;
-
-DELETE FROM public.friendships f
-USING public.friendships dupe
-WHERE f.ctid < dupe.ctid
-  AND f.user_id = dupe.user_id
-  AND f.friend_user_id = dupe.friend_user_id;
+LOCK TABLE public.friendships IN ACCESS EXCLUSIVE MODE;
 
 ALTER TABLE public.friendships
   DROP CONSTRAINT IF EXISTS chk_friendships_canonical_pair;
+
+CREATE TEMP TABLE tmp_friendships_canonical AS
+SELECT DISTINCT
+  LEAST(user_id, friend_user_id) AS user_id,
+  GREATEST(user_id, friend_user_id) AS friend_user_id
+FROM public.friendships
+WHERE user_id <> friend_user_id;
+
+DELETE FROM public.friendships;
+
+INSERT INTO public.friendships (user_id, friend_user_id)
+SELECT user_id, friend_user_id
+FROM tmp_friendships_canonical
+ORDER BY user_id, friend_user_id
+ON CONFLICT (user_id, friend_user_id) DO NOTHING;
 
 ALTER TABLE public.friendships
   ADD CONSTRAINT chk_friendships_canonical_pair
