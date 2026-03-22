30d8ef2998f8dd8b1e179afe617fcd30042e2e7e
Codingwithnicotine <casanajemcarlo@gmail.com>
2026-03-22 11:43:35 +0800

    migrations fixed

--- migrations/2026-03-21_friend_pair_state_rebuild.sql
+++ migrations/2026-03-21_friend_pair_state_rebuild.sql
@@ -3,6 +3,8 @@ BEGIN;
 ALTER TABLE public.friendships
   DROP CONSTRAINT IF EXISTS chk_friendships_canonical_pair;
 
+LOCK TABLE public.friendships IN ACCESS EXCLUSIVE MODE;
+
 CREATE TEMP TABLE tmp_friendships_canonical AS
 SELECT DISTINCT
   LEAST(user_id, friend_user_id) AS user_id,
@@ -15,7 +17,8 @@ DELETE FROM public.friendships;
 INSERT INTO public.friendships (user_id, friend_user_id)
 SELECT user_id, friend_user_id
 FROM tmp_friendships_canonical
-ORDER BY user_id, friend_user_id;
+ORDER BY user_id, friend_user_id
+ON CONFLICT (user_id, friend_user_id) DO NOTHING;
 
 ALTER TABLE public.friendships
   ADD CONSTRAINT chk_friendships_canonical_pair
